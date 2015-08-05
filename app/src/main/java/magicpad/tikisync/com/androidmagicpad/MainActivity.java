f4db17d9d9e7e63f7726f89e1fdf89028f796163
Admin <ngathet3@gmail.com>
2015-08-05 10:30:47 +0630

    First

--- app/src/main/java/magicpad/tikisync/com/androidmagicpad/MainActivity.java
+++ app/src/main/java/magicpad/tikisync/com/androidmagicpad/MainActivity.java
@@ -6,6 +6,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -14,6 +19,28 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hellogit", Toast.LENGTH_SHORT).show();
+        Thread tthread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Connect();
+            }
+        });
+        tthread.start();
+
+    }
+
+    public void Connect() {
+        try {
+            Socket SOCK = new Socket("192.168.0.43", 333);
+            PrintWriter pw = new PrintWriter(SOCK.getOutputStream());
+            pw.println("FROM ANDROID!");
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
